685e26cef6473a5acb658c77b272f8f488a0597c
kdrzazga <krzysztof.drzazga@op.pl>
2017-01-19 21:11:10 +0100

    Create AllUnitTests.java

---
+++ miscallenous/AllUnitTests.java
@@ -0,0 +1,9 @@
+package miscallenous;
+
+public class AllUnitTests {
+    public static void main(String args[])
+    {
+        logic.test.BoardTrackVehicleTest.main(args);
+        presentation.test.KeyboardInputTest.main(args);
+    }    
+}
