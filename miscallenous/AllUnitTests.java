994e4035d9729c31c2ebba98ecac0378a9725ce5
krzysztof.drzazga@capgemini.com <krzysztof.drzazga@op.pl>
2017-11-26 18:56:48 +0100

    Removing code to enable master merge with develop

--- miscallenous/AllUnitTests.java
+++
@@ -1,9 +0,0 @@
-package miscallenous;
-
-public class AllUnitTests {
-    public static void main(String args[])
-    {
-        logic.test.BoardTrackVehicleTest.main(args);
-        presentation.test.KeyboardInputTest.main(args);
-    }    
-}
